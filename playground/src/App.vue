392fd4501956b6d3e38271375c76976aeeb23868
Simon-He95 <674949287@qq.com>
2022-09-17 00:55:12 +0800

    chore: init

---
+++ playground/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { createElement } from '../../src/component'
+function clickHandler(e: MouseEvent) {
+  createElement(e)
+}
+</script>
+
+<template>
+  <main
+    font-sans
+    p="x-4 y-10"
+    text="center gray-700 dark:gray-200"
+    @mousemove="clickHandler"
+  >
+    <my-component />
+    <router-view />
+    <Footer />
+  </main>
+</template>
